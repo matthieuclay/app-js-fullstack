9f83b62c6562c5a4bd896e863e5f0e6643bab628
Matthieu Clay <matthieu.clay@outlook.fr>
2023-04-04 16:15:38 +0200

    add controller and model

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,28 +1,19 @@
 const express = require('express');
+const {
+	setPosts,
+	getPosts,
+	editPost,
+	deletePost,
+	likePost,
+	dislikePost,
+} = require('../controllers/post.controller');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.json({ message: 'voici les données' });
-});
-
-router.post('/', (req, res) => {
-	res.json({ message: req.body.message });
-});
-
-router.put('/:id', (req, res) => {
-	res.json({ messageId: req.params.id });
-});
-
-router.delete('/:id', (req, res) => {
-	res.json({ message: 'Post supprimé, id : ' + req.params.id });
-});
-
-router.patch('/like-post/:id', (req, res) => {
-	res.json({ message: 'Post liké, id : ' + req.params.id });
-});
-
-router.patch('/dislike-post/:id', (req, res) => {
-	res.json({ message: 'Post disliké, id : ' + req.params.id });
-});
+router.get('/', getPosts);
+router.post('/', setPosts);
+router.put('/:id', editPost);
+router.delete('/:id', deletePost);
+router.patch('/like-post/:id', likePost);
+router.patch('/dislike-post/:id', dislikePost);
 
 module.exports = router;
